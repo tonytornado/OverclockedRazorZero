1afa48fd03f93a89914b2d73fb419238f87f59de
Tony Thigpen <tonytatsumaki@live.com>
2018-01-23 01:47:39 -0500

    General bits

--- OverclockedRazor/wwwroot/js/site.js
+++ OverclockedRazor/wwwroot/js/site.js
@@ -1,1 +1,15 @@
 ﻿// Write your Javascript code.
+
+// GOOGLE MAPS CODE //
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(38.200946, -85.601353),
+        zoom: 10,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+// SLIDE IN CODE //
+var scrollText = $('body div.parallax');
+
+scrollText.delay(2000).fadeIn(2000);
