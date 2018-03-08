d80ee68554c97e1a38ed6ce09e6fa80d09384039
Tony Thigpen <tonytatsumaki@live.com>
2018-03-08 17:52:26 -0500

    Jumbotron on the front page.

--- OverclockedRazor/wwwroot/js/site.js
+++ OverclockedRazor/wwwroot/js/site.js
@@ -37,3 +37,11 @@ function revealOnScroll() {
 $(document).ready(function () {
     $('.parallax-title').css("height",window.innerHeight);
 });
+
+
+var o = {
+    "point": {
+        "X": 0,
+        "Y": 0
+    },
+};
